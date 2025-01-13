c10f29f6036342923d80eb8c0c10af53080b2c67
Jake Hearn <jakehearn1997@gmail.com>
2025-01-13 18:38:43 +0000

    remove venv

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+from datetime import datetime 
+import calendar
+
+app = Flask(__name__)
+currentDate = datetime.now()
+
+@app.route('/1000km', methods=['GET'])
+def thousandKmPY():
+    currentDay = currentDate.timetuple().tm_yday
+    currentAverage = (1000/365)*currentDay
+
+    return jsonify({
+        'dayOfYear': currentDay,
+        'currentYearAverage': currentAverage
+    })
+    
+@app.route('/100km', methods= ['GET'])
+def hundredKmPM():
+    year = currentDate.year
+    month = currentDate.month
+    day = currentDate.day
+
+    days = calendar.monthrange(year,month)[1]
+    dayAverage = 100 / days 
+    currentAverage = dayAverage * day
+    
+    return jsonify({
+        'dayOfMonth': day,
+        'requiredAverage': dayAverage,
+        'currentMonthAverage': currentAverage
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
